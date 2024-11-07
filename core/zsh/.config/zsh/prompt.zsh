4834cf2357b41a63e7a054a0d3c9e91e99eaa5e9
Łukasz Strzępek <lstrzepek@users.noreply.github.com>
2024-11-07 22:49:49 +0100

    Reduced git info in dev workflow

--- core/zsh/.config/zsh/prompt.zsh
+++ core/zsh/.config/zsh/prompt.zsh
@@ -38,24 +38,25 @@ function setup_minimal_prompt() {
   
   # Function to be executed before each prompt
   function precmd() {
-    vcs_info
-    
-    # Get current directory (just the last component)
+#    vcs_info
+#    
+#    # Get current directory (just the last component)
     local current_dir="%~"
-    
-    # If we're in a git repo, prepare the branch display
-    local git_info=""
-    if [[ -n $vcs_info_msg_0_ ]]; then
-      # Check if there are any changes in the repo
-      local git_status=""
-      if [[ -n $(git status --porcelain 2>/dev/null) ]]; then
-        git_status="%F{magenta}*%f"
-      fi
-      git_info=" %F{blue}[%f ${vcs_info_msg_0_}%f${git_status}%F{blue}]%f"
-    fi
+#    
+#    # If we're in a git repo, prepare the branch display
+#    local git_info=""
+#    if [[ -n $vcs_info_msg_0_ ]]; then
+#      # Check if there are any changes in the repo
+#      local git_status=""
+#      if [[ -n $(git status --porcelain 2>/dev/null) ]]; then
+#        git_status="%F{magenta}*%f"
+#      fi
+#      git_info=" %F{blue}[%f ${vcs_info_msg_0_}%f${git_status}%F{blue}]%f"
+#    fi
     
     # Set the prompt: cyan directory, blue git branch with magenta status indicator
-    PROMPT="%F{cyan}${current_dir}%f${git_info} %F{green}%f "
+    #PROMPT="%F{cyan}${current_dir}%f${git_info} %F{green}%f "
+    PROMPT="%F{cyan}${current_dir}%f %F{green}%f "
   }
 }
 
