1f30da287a2a17dab51b6300d640e6d27d5cc17e
Łukasz Strzępek <lstrzepek@users.noreply.github.com>
2024-11-13 22:42:20 +0100

    Switch back to my custom nvim config

--- nvim-custom/.config/nvim/lua/plugins/which-key.lua
+++ nvim-custom/.config/nvim/lua/plugins/which-key.lua
@@ -7,15 +7,26 @@ return {
   end,
   opts = {
     spec = {
-      --      {
-      --        mode = { 'n', 'v' },
-      { '<leader>f', group = 'find' },
-      { '<leader>c', group = 'code' },
-      { '<leader>g', group = 'git' },
-      { '<leader>b', group = 'buffers' },
-      { "[",         group = "prev" },
-      { "]",         group = "next" },
-    }
+        { '<leader>b', group = '[B]uffer' },
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      },
+    --spec = {
+    --  --      {
+    --  --        mode = { 'n', 'v' },
+    --  { '<leader>f', group = 'find' },
+    --  { '<leader>c', group = 'code' },
+    --  { '<leader>e', group = 'explore' },
+    --  { '<leader>g', group = 'git' },
+    --  { '<leader>b', group = 'buffers' },
+    --  { "[",         group = "prev" },
+    --  { "]",         group = "next" },
+    --}
     --    }
     --    -- your configuration comes here
     --    -- or leave it empty to use the default settings
